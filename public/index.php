3a872149708555891a1a0c6e39787cafdb8c7feb
Abdullah <abdullah@example.com>
2026-04-10 18:16:46 +0500

    Fix Laravel bootstrap and add required runtime directories

--- public/index.php
+++ public/index.php
@@ -2,36 +2,10 @@
 
 declare(strict_types=1);
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
-|
-*/
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we'll send the response back
-| to this client's browser allowing them to enjoy our application.
-|
-*/
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(Illuminate\Http\Request::capture());
